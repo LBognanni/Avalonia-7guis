f8d60367b82a633adcf6a1fc8ab20d43cd7eea94
Loris Bognanni <loris@codemade.co.uk>
2020-11-14 20:55:49 +0000

    Add Temperature converter

---
+++ TemperatureConverter/MainWindow.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TemperatureConverter
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.DataContext = new TemperatureViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TemperatureConverter
+{
+    class TemperatureViewModel : INotifyPropertyChanged
+    {
+        private decimal _celsius;
+        private decimal _farenheit;
+        private bool _isUpdating;
+
+        public TemperatureViewModel() => Celsius = 0;
+
+        public decimal Celsius
+        {
+            get => _celsius; 
+            set
+            {
+                if (_celsius != value)
+                {
+                    _celsius = value;
+                    OnPropertyChanged();
+
+                    if (!_isUpdating)
+                    {
+                        _isUpdating = true;
+                        Farenheit = Celsius * (9M / 5M) + 32M;
+                        _isUpdating = false;
+                    }
+                }
+            }
+        }
+
+        public decimal Farenheit
+        {
+            get => _farenheit; 
+            set
+            {
+                if (_farenheit != value)
+                {
+                    _farenheit = value;
+                    OnPropertyChanged();
+
+                    if (!_isUpdating)
+                    {
+                        _isUpdating = true;
+                        Celsius = (value - 32M) * (5.0M / 9.0M);
+                        _isUpdating = false;
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
