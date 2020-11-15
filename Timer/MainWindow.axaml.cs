61761338c263450a49e44530496fca2942214ba9
Loris Bognanni <loris@codemade.co.uk>
2020-11-15 11:34:12 +0000

    Add Timer app

---
+++ Timer/MainWindow.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Timer
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.DataContext = new TimerViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Timer/TimerViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Timer
+{
+    class TimerViewModel : INotifyPropertyChanged
+    {
+        private System.Timers.Timer _timer;
+        private int _duration;
+        private decimal _elapsed;
+
+        public TimerViewModel()
+        {
+            Duration = 10;
+        }
+
+        public int Duration
+        {
+            get => _duration;
+            set
+            {
+                if (_duration != value)
+                {
+                    _duration = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public decimal Elapsed
+        {
+            get => _elapsed;
+            set
+            {
+                if (_elapsed != value)
+                {
+                    _elapsed = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            Elapsed = 0;
+            _timer = new System.Timers.Timer(100);
+            _timer.Elapsed += (s, a) =>
+            {
+                if (Elapsed < Duration)
+                {
+                    Elapsed += 0.1M;
+                }
+            };
+            _timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
